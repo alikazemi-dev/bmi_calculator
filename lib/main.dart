4ee424e2559a9a499bb8f43e9a7ddeb265655d95
Mohammad Ali Kazemi <mak.iran.96@gmail.com>
2023-11-30 13:14:02 +0330

    1111

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(Application());
 }
 
+class Application extends StatelessWidget {
+  const Application({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            child: Text('Ali Kazemi'),
+          ),
+        ),
+      ),
+    );
+  }
+}
